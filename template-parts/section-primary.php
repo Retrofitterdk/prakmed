3b458c313abacaab2589d9d14ae03ce8b04eb724
anpu <anpu@dagensmedicin.dk>
2016-12-22 13:41:28 +0100

    added sections taxonomy in the loop

--- template-parts/section-primary.php
+++ template-parts/section-primary.php
@@ -12,7 +12,7 @@ $sections = get_terms([
     'taxonomy' => 'section',
     'hide_empty' => false,
 ]);
-//var_dump($sections);
+var_dump($sections);
 $output = '<ul>';
 foreach($sections as $section) {
   $output .= '<li>' . $section->name . '</li>';
@@ -21,6 +21,24 @@ $output .= '</ul>';
 echo $output;
 ?>
 <hr />
+
+<!-- Display all subjects -->
+<h1>All subjects</h1>
+<?php
+$subjects = get_terms([
+    'taxonomy' => 'prakmed_subjects',
+    'hide_empty' => false,
+]);
+//var_dump($sections);
+$output = '<ul>';
+foreach($subjects as $subject) {
+  $output .= '<li>' . $subject->name . '</li>';
+}
+$output .= '</ul>';
+echo $output;
+?>
+<hr />
+
 <!-- Cycle through all posts [THE LOOP] -->
 <?php while ( $loop->have_posts() ) : $loop->the_post();?>
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -33,6 +51,9 @@ echo $output;
       <!-- Display custom post type prakmed_article and taxonomies -->
       <strong>Article title: </strong>
       <?php the_title(); ?><br />
+
+      <strong>Sections:</strong>
+      <?php the_terms( $post->ID, 'section', '' ); ?><br />
 
       <strong>Subject:</strong>
       <?php the_terms( $post->ID, 'prakmed_subjects', '' ); ?><br />
@@ -47,7 +68,10 @@ echo $output;
       <?php the_terms( $post->ID, 'ICD-10', '' ); ?><br />
 
       <strong>Related drugs </strong>
-      <?php get_post_meta( get_the_id(), 'prakmed_related_drugs', '' ); ?><br />
+      <?php echo get_post_meta( $post->ID, 'prakmed_related_drugs', true ); ?><br />
+
+      <strong>Disease Classification </strong>
+      <?php echo get_post_meta( $post->ID, 'prakmed_disease_classifaction', true ); ?><br />
 
     </header>
 
