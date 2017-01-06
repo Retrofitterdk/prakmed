f02851a985fc5225702eb2e30e3b3907e391cdfe
anpu <anpu@dagensmedicin.dk>
2017-01-06 13:24:48 +0100

    now using while-loop instead of arrays

--- single-prakmed_article.php
+++ single-prakmed_article.php
@@ -12,46 +12,33 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		global $post;
-		$IDC_10_terms = get_the_terms( $post->ID, 'ICD-10', '' );
-		$ICPC_2_terms = get_the_terms( $post->ID, 'ICPC-2', '' );
-		$DSM_5_terms = get_the_terms( $post->ID, 'DSM-5', '' );
 
+			<?php
+			global $post;
+				while ( have_posts() ) : the_post();
+				$IDC_10_terms = get_the_term_list( $post->ID, 'ICD-10', __( 'ICD-10: ', 'prakmed' ) );
+				$ICPC_2_terms = get_the_term_list( $post->ID, 'ICPC-2', __( 'ICPC-2: ', 'prakmed' ) );
 
-		$output = '';
-		$output .= '<article id=' . $post->ID . ' class="handbook-article">';
-		$output .= "<figure>" . get_the_post_thumbnail( $post->ID ) . "</figure>";
-		$output .= '<header>';
-		$output .= '<h1>' . $post->post_title . '</h1>';
-
-		$output .= '<p class="taxonomies">';
-		$output .= '<strong>ICD-10</strong> ';
-		foreach($IDC_10_terms as $IDC_10_term) {
-		$output .= $IDC_10_term->name . ' ';
-		}
-		$output .= ' <strong>ICPC-2</strong> ';
-		foreach($ICPC_2_terms as $ICPC_2_term) {
-		$output .= $ICPC_2_term->name . ' ';
-		}
-		$output .= ' <strong>DSM-5</strong> ';
-		foreach($DSM_5_terms as $DSM_5_term) {
-		$output .= $DSM_5_term->name . ' ';
-		}
-		$output .= '</p>';
-		$output .= '</header>';
-
-		$output .= '<div class="article-content">';
-		$output .= $post->post_content;
-		$output .= '</article>';
-
-		echo $output;
-
-		echo "<div class='pagination'>";
-		echo  previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous feature', 'prakmed' ) . '</span> %title' );
-		echo  next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next feature', 'prakmed' ) . '</span>' );
-		echo "</div>";
-		?>
+				echo "<article id=". $post->ID ." class='handbook-article'>";
+				echo "<figure>";
+				the_post_thumbnail();
+				echo "</figure>";
+				echo "<header><h1>";
+				the_title();
+				echo "<p class='taxonomies'>";
+				if ( $IDC_10_terms ) {
+					printf( '' . __( '%1$s', 'prakmed' ) . '', $IDC_10_terms );
+				}
+				if ( $ICPC_2_terms ) {
+					printf( '' . __( '%1$s', 'prakmed' ) . '', $ICPC_2_terms );
+				}
+				echo "</p>";
+				echo "</h1></header>";
+				echo "<div class='article-content'>";
+				the_content();
+				echo "</div>";
+				endwhile; // End of the loop.
+			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
