19ae1915be2dc31e12b7ee6fd71ac998a00a6fd8
sbangnielsen <sbn@dagensmedicin.dk>
2017-01-19 16:29:22 +0100

    add meta template tag

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * Custom template tags for this theme.
- *
- * Eventually, some of the functionality here could be replaced by core features.
- *
- * @package PrakMed
- */
+* Custom template tags for this theme.
+*
+* Eventually, some of the functionality here could be replaced by core features.
+*
+* @package PrakMed
+*/
 
 if ( ! function_exists( 'prakmed_posted_on' ) ) :
-/**
- * Prints HTML with meta information for the current post-date/time and author.
- */
-function prakmed_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
+	/**
+	* Prints HTML with meta information for the current post-date/time and author.
+	*/
+	function prakmed_posted_on() {
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
 
-	$time_string = sprintf( $time_string,
+		$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
 		esc_attr( get_the_modified_date( 'c' ) ),
@@ -39,50 +39,74 @@ function prakmed_posted_on() {
 }
 endif;
 
+if ( ! function_exists( 'prakmed_entry_meta' ) ) :
+	function prakmed_entry_meta() {
+		if ( 'prakmed_article' === get_post_type() ) {
+			$prakmed_section = get_the_term_list( get_the_ID(), 'section', __( '<strong>Section: </strong>', 'prakmed' ),', ',' ' );
+			$IDC_10_terms = get_the_term_list( get_the_ID(), 'ICD-10', __( '<strong>ICD-10: </strong>', 'prakmed' ),', ',' ' );
+			$ICPC_2_terms = get_the_term_list( get_the_ID(), 'ICPC-2', __( '<strong>ICPC-2: </strong>', 'prakmed' ),', ',' ' );
+			$DSM_5_terms = get_the_term_list( get_the_ID(), 'DSM-5', __( '<strong>DSM-5: </strong>', 'prakmed' ),', ',' ' );
+			if ( $prakmed_section ) {
+				printf( '<span class="Section">' . __( '%1$s', 'prakmed' ) . '</span>', $prakmed_section );
+			}
+			if ( $IDC_10_terms ) {
+				printf( '<span class="ICD-10">' . __( '%1$s', 'prakmed' ) . '</span>', $IDC_10_terms );
+			}
+			if ( $ICPC_2_terms ) {
+				printf( '<span class="ICPC-2">' . __( '%1$s', 'prakmed' ) . '</span>', $ICPC_2_terms );
+			}
+			if ( $DSM_5_terms ) {
+				printf( '<span class="DSM-5">' . __( '%1$s', 'prakmed' ) . '</span>', $DSM_5_terms );
+			}
+		}
+	}
+endif;
+
+
 if ( ! function_exists( 'prakmed_entry_footer' ) ) :
-/**
- * Prints HTML with meta information for the categories, tags and comments.
- */
-function prakmed_entry_footer() {
-	// Hide category and tag text for pages.
-	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'prakmed' ) );
-		if ( $categories_list && prakmed_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'prakmed' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+	/**
+	* Prints HTML with meta information for the categories, tags and comments.
+	*/
+	function prakmed_entry_footer() {
+		// Hide category and tag text for pages.
+		if ( 'post' === get_post_type() ) {
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( esc_html__( ', ', 'prakmed' ) );
+			if ( $categories_list && prakmed_categorized_blog() ) {
+				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'prakmed' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+			}
+
+			/* translators: used between list items, there is a space after the comma */
+			$tags_list = get_the_tag_list( '', esc_html__( ', ', 'prakmed' ) );
+			if ( $tags_list ) {
+				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'prakmed' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+			}
 		}
 
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'prakmed' ) );
-		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'prakmed' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+			echo '<span class="comments-link">';
+			/* translators: %s: post title */
+			comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'prakmed' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
+			echo '</span>';
 		}
-	}
 
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		/* translators: %s: post title */
-		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'prakmed' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
-		echo '</span>';
+		edit_post_link(
+			sprintf(
+				/* translators: %s: Name of current post */
+				esc_html__( 'Edit %s', 'prakmed' ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
 	}
-
-	edit_post_link(
-		sprintf(
-			/* translators: %s: Name of current post */
-			esc_html__( 'Edit %s', 'prakmed' ),
-			the_title( '<span class="screen-reader-text">"', '"</span>', false )
-		),
-		'<span class="edit-link">',
-		'</span>'
-	);
-}
 endif;
 
 /**
- * Returns true if a blog has more than 1 category.
- *
- * @return bool
- */
+* Returns true if a blog has more than 1 category.
+*
+* @return bool
+*/
 function prakmed_categorized_blog() {
 	if ( false === ( $all_the_cool_cats = get_transient( 'prakmed_categories' ) ) ) {
 		// Create an array of all the categories that are attached to posts.
@@ -109,8 +133,8 @@ function prakmed_categorized_blog() {
 }
 
 /**
- * Flush out the transients used in prakmed_categorized_blog.
- */
+* Flush out the transients used in prakmed_categorized_blog.
+*/
 function prakmed_category_transient_flusher() {
 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 		return;
