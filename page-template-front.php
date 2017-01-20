094f445583023bf1584fb13533a0c117dbebf5f7
sbangnielsen <sbn@dagensmedicin.dk>
2017-01-20 10:09:48 +0100

    Create frontpage template with search and display of all sections

---
+++ page-template-front.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* Template Name: Frontpage
+*
+* This page template is a custom frontpage
+*
+* @link https://codex.wordpress.org/Template_Hierarchy
+*
+* @package PrakMed
+*/
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div id="search" class="search">
+			<?php get_search_form(); ?>
+		</div>
+		<div id="sections" class="taxonomy-list">
+			<?php prakmed_the_sections(); ?>
+			</div
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	<?php
+	get_sidebar();
+	get_footer();
