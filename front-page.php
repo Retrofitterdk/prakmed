dbd3c298dfb868dfeaee1f41018d5e6d15423ab0
sbangnielsen <sbn@dagensmedicin.dk>
2017-01-20 08:32:08 +0100

    Remove comments from pages

---
+++ front-page.php
@@ -0,0 +1,20 @@
+
+<?php
+/**
+ * The front page template file
+ *
+ * Just a script to make sure editors always gets to choose
+ * between static front page and a dynamic homepage
+ *
+* @package WordPress
+* @subpackage prakmed
+* @since prakmed 1.0
+* @link https://codex.wordpress.org/Template_Hierarchy
+*
+*/
+
+if ( 'posts' == get_option( 'show_on_front' ) ) { ?>
+    <?php include( get_home_template() );
+} else { ?>
+    <?php include( get_page_template() );
+}
