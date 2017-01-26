1c939c81774ab185cec9bbe47c29207db546a2d2
sbangnielsen <sbn@dagensmedicin.dk>
2017-01-26 03:03:07 +0100

    Refactor aside

--- sidebar.php
+++ sidebar.php
@@ -1,21 +1,20 @@
 <?php
 /**
- * The sidebar containing the main widget area.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package PrakMed
- */
- ?>
- <nav id="primary-menu" class="mobile hide primary-menu sidebar three columns">
- 	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-</nav>
-<?php
-
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+* The sidebar containing the main widget area.
+*
+* @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+*
+* @package PrakMed
+*/
 ?>
-<aside id="secondary" class="widget-area three columns" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<aside id="secondary" class="sidebar widget-area three columns" role="complementary">
+  <nav id="primary-menu-container" class="mobile hide primary-menu" role="navigation">
+  <?php get_template_part( 'template-parts/menu', 'primary' ); ?>
+  </nav><!-- #footer-navigation -->
+  <?php
+  if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+    return;
+  }
+  dynamic_sidebar( 'sidebar-1' );
+  ?>
 </aside><!-- #secondary -->
