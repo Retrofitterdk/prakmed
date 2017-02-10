d0b96c0f422282441020f39bac88674f77114b3d
sbangnielsen <sbn@dagensmedicin.dk>
2017-02-10 14:21:03 +0100

    Fix translations

--- template-parts/login.php
+++ template-parts/login.php
@@ -19,7 +19,7 @@ if ( ! is_user_logged_in() ) { // Display WordPress login form:	?>
 	</div>
 	<?php	} else { ?>
 		<div id="user-box" class="login-wrapper header bg dark">
-			<a class="button" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ) ?>" title="<?php _e('My Account','prakmed') ?>"><?php _e('My Account','dagens') ?></a>
+			<a class="button" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ) ?>" title="<?php _e('My Account','prakmed') ?>"><?php _e('My Account','prakmed') ?></a>
 			<div class="button"><?php wp_loginout( home_url() ) // Display "Log Out" link. ?></div>
 		</div>
 		<?php } ?>
