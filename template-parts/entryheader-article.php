2ce0ce083b9dfa67fa2957748959da4c623a6870
sbangnielsen <sbn@dagensmedicin.dk>
2017-02-27 00:22:56 +0100

    Improve titel-link on archive pages

--- template-parts/entryheader-article.php
+++ template-parts/entryheader-article.php
@@ -2,20 +2,19 @@
 if ( is_singular() ) :
 	?>
 	<header class="entry-header singular">
-		<?php
-		the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			?>
-			<header class="entry-header">
-				<?php
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
-			?>
-			<div class="entry-meta">
-				<?php
-				prakmed_entry_meta();
-				prakmed_posted_by();
-prakmed_last_edit()
-?>	
-			</div>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header>
+<?php	else : ?>
+	<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+		<header class="entry-header">
+			<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 		</header>
+	</a>
+<?php endif; ?>
+<div class="entry-meta">
+	<?php
+	prakmed_entry_meta();
+	prakmed_posted_by();
+	prakmed_last_edit()
+	?>
+</div>
