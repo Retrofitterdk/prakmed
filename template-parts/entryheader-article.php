c944af02cf7a67ebb91164136eec089c55cda4a4
sbangnielsen <sbn@dagensmedicin.dk>
2017-02-28 13:52:47 +0100

    Make links more visible on archive pages

--- template-parts/entryheader-article.php
+++ template-parts/entryheader-article.php
@@ -1,20 +1,19 @@
-<?php
-if ( is_singular() ) :
-	?>
+<?php if ( is_singular() ) : ?>
 	<header class="entry-header singular">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header>
-<?php	else : ?>
-	<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
-		<header class="entry-header">
-			<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+	<?php	else : ?>
+		<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+			<header class="entry-header">
+				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+			<?php endif; ?>
 		</header>
-	</a>
-<?php endif; ?>
-<div class="entry-meta">
-	<?php
-	prakmed_entry_meta();
-	prakmed_posted_by();
-	prakmed_last_edit()
-	?>
-</div>
+			<div class="entry-meta">
+				<?php
+				prakmed_entry_meta();
+				prakmed_posted_by();
+				prakmed_last_edit()
+				?>
+			</div>
+			<?php if ( !is_singular() ) :	?>
+			</a>
+		<?php endif; ?>
