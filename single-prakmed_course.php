ea7c0ca87e6168b0e1ad8a9d93ed29e81daea9da
sbangnielsen <sbn@dagensmedicin.dk>
2020-09-12 15:28:21 +0200

    Add course template

---
+++ single-prakmed_course.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* The template for displaying single custom post type prakmed_course.
+*
+* @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+*
+* @package PrakMed
+*/
+$hello_function = new CourseController();
+$hello_function->course();
