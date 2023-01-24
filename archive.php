09496e156c9b37c209fa0ee4e07cb8717635fe48
Retrofitterdk <sbn@retrofitter.dk>
2023-01-24 11:37:02 +0100

    Setup default article archive

--- archive.php
+++ archive.php
@@ -26,7 +26,7 @@ get_header(); ?>
 			the_archive_description( '<div class="archive-description">', '</div>' );
 			?>
 		</header><!-- .page-header -->
-		<div id="content" class="">
+		<div id="content" class="article-archive">
 		<?php
 		/* Start the Loop */
 		while ( have_posts() ) : the_post();
@@ -36,7 +36,7 @@ get_header(); ?>
 		* If you want to override this in a child theme, then include a file
 		* called content-___.php (where ___ is the Post Format name) and that will be used instead.
 		*/
-		get_template_part( 'template-parts/content', get_post_format() );
+		get_template_part( 'template-parts/content', 'article-archive' );
 
 	endwhile;
 	?>
